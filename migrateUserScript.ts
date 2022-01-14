62a146bf6c1037e9fa7da2458fa10d0046984ee0
trey-rosius <treyrosius@gmail.com>
2022-01-14 17:38:49 +0100

    more updates

--- migrateUserScript.ts
+++ migrateUserScript.ts
@@ -1,24 +1,33 @@
-import FusionAuthClient from "@fusionauth/typescript-client";
+import { FusionAuthClient } from "@fusionauth/typescript-client";
 
 import JSONStream from "JSONStream";
+import ksuid from "ksuid";
 
-var fs = require("fs");
+import fs from "fs";
 
-let applicationId: string;
+export const uuid = (): string => {
+  return ksuid.randomSync().string;
+};
+let applicationId: string = "a3c87de8-92fe-4739-8d4e-0bcf75ea03bf";
 let importUsers: Object[] = [];
 let fusionAuthUser: {};
 var stream = fs.createReadStream("datasource/users.json", {
   flags: "r",
   encoding: "utf-8",
 });
+var client = new FusionAuthClient(
+  applicationId,
+  "auth.dev.axis.lotterydev.com"
+);
 stream.pipe(JSONStream.parse("*")).on("data", (d: {}) => {
-  console.log(d["email"]);
-  console.log(d["phoneVerified"]);
-  console.log(d["emailVerified"]);
-  console.log(d["_id"]["$oid"]);
+  //console.log(d["email"]);
+  //console.log(d["phoneVerified"]);
+  // console.log(d["emailVerified"]);
+  // console.log(d["_id"]["$oid"]);
   fusionAuthUser = {
     id: d["_id"]["$oid"],
     email: d["email"],
+    password: uuid(),
     registrations: [
       {
         applicationID: applicationId,
@@ -31,6 +40,8 @@ stream.pipe(JSONStream.parse("*")).on("data", (d: {}) => {
   };
   importUsers.push(fusionAuthUser);
 });
-var client = new FusionAuthClient(applicationId, "https://local.fusionauth.io");
-
-client.importUsers({ users: importUsers });
+try {
+  client.importUsers({ users: importUsers });
+} catch (error) {
+  console.log(error);
+}
--- output/migrateUserScript.js
+++ output/migrateUserScript.js
@@ -1,29 +1,42 @@
-var fs = require("fs");
-var JSONStream = require("JSONStream");
-let applicationId;
+import { FusionAuthClient } from "@fusionauth/typescript-client";
+import JSONStream from "JSONStream";
+import ksuid from "ksuid";
+import fs from "fs";
+export const uuid = () => {
+    return ksuid.randomSync().string;
+};
+let applicationId = "a3c87de8-92fe-4739-8d4e-0bcf75ea03bf";
 let importUsers = [];
 let fusionAuthUser;
 var stream = fs.createReadStream("datasource/users.json", {
-  flags: "r",
-  encoding: "utf-8",
+    flags: "r",
+    encoding: "utf-8",
 });
+var client = new FusionAuthClient(applicationId, "auth.dev.axis.lotterydev.com");
 stream.pipe(JSONStream.parse("*")).on("data", (d) => {
-  console.log(d["email"]);
-  console.log(d["phoneVerified"]);
-  console.log(d["emailVerified"]);
-  console.log(d["_id"]["$oid"]);
-  fusionAuthUser = {
-    id: d["_id"]["$oid"],
-    email: d["email"],
-    registrations: [
-      {
-        applicationID: applicationId,
-        data: {
-          phoneVerified: d["phoneVerified"],
-          emailVerified: d["emailVerified"],
-        },
-      },
-    ],
-  };
-  importUsers.push(fusionAuthUser);
+    //console.log(d["email"]);
+    //console.log(d["phoneVerified"]);
+    // console.log(d["emailVerified"]);
+    // console.log(d["_id"]["$oid"]);
+    fusionAuthUser = {
+        id: d["_id"]["$oid"],
+        email: d["email"],
+        password: uuid(),
+        registrations: [
+            {
+                applicationID: applicationId,
+                data: {
+                    phoneVerified: d["phoneVerified"],
+                    emailVerified: d["emailVerified"],
+                },
+            },
+        ],
+    };
+    importUsers.push(fusionAuthUser);
 });
+try {
+    client.importUsers({ users: importUsers });
+}
+catch (error) {
+    console.log(error);
+}
